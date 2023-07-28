faa1bb327534ca01d0894860cb4adef5160d2690
shavilya <shavilyarajput50@gmail.com>
2023-07-28 10:22:40 +0530

    Hyper-parameter Training

--- src/utils.py
+++ src/utils.py
@@ -1,48 +1,58 @@
-# This python file will have all the common functionalities which can used in the project. 
-
-
-
-import os 
-import sys 
-
-import dill 
+import os
+import sys
 
 import numpy as np 
-import pandas as pd 
-
+import pandas as pd
+import dill
+import pickle
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
-def save_object(file_path , obj) : 
-    try : 
+def save_object(file_path, obj):
+    try:
         dir_path = os.path.dirname(file_path)
-        os.makedirs(dir_path , exist_ok = True)
 
-        with open(file_path , "wb") as file_obj : 
-            dill.dump(obj , file_obj)
+        os.makedirs(dir_path, exist_ok=True)
 
-    except Exception as e : 
-        raise CustomException(e,sys)
-     
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
 
-def evaluate_models(x_train , y_train , x_test , y_test , models, params) : 
-    try : 
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(x_train, y_train,x_test,y_test,models,params):
+    try:
         report = {}
 
-        for i in range(len(list(models))) : 
+        for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(x_train , y_train) # Train model. 
+            para=params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(x_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(x_train)
+
             y_test_pred = model.predict(x_test)
 
-            train_model_score = r2_score(y_train,y_train_pred)
-            test_model_score = r2_score(y_test,y_test_pred)
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
-        return report 
-    
-    except Exception as e : 
-        raise CustomException(e,sys)
-    
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+    except Exception as e:
+        raise CustomException(e, sys)
